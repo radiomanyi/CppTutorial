4d1c0fde211a0754b693698c463f1d3313105347
radiomanyi <pyzyf1983@gmail.com>
2021-03-14 13:24:44 +0800

    Enter

---
+++ calculator/calculator.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+class Calculator{
+    public:
+    int Calculate (int, int, char);
+};
+
+int main()
+{
+    int x, y, result;
+    char oper;
+    cout << "Hello! I am Calculator!"<<endl;
+    cout << "Please enter num1 operator num2: "<<endl;
+    cin >>x>>y>>oper;
+    Calculator c;
+    result = c.Calculate(x, y, oper);
+    cout <<"result is: "<<result<<endl;
+    cin.ignore();
+    cin.get();
+    return 0;
+}
+
+int Calculator::Calculate(int x, int y, char oper)
+{
+    switch (oper)
+    {
+    case '+':
+        return x + y;
+    case '-':
+        return x - y;
+    case '*':
+        return x * y;
+    case '/':
+        if (y != 0)
+            return x / y;
+    default:
+        cin >> x >> y >> oper;
+        Calculate(x, y, oper);
+    }
+}
+
---
+++ resistor/resistor.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    double r_1,r_2,r_s;
+    printf("Please enter the values of r1 and r2: ");
+    scanf("%lf %lf",&r_1,&r_2);
+    r_s = r_1 + r_2;
+    printf("The sum resistance is:%4.1lf\n",r_s);
+    return 0;
+}
