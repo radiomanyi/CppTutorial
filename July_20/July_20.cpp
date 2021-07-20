1b8702799f6a6cb88a3dbe744d47919314e8de7c
radiomanyi <pyzyf1983@gmail.com>
2021-07-20 19:39:47 +0800

    Self-Define Name

--- July_20/July_20.cpp
+++ July_20/July_20.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
 using namespace std;
 int main()
-{
+{   char name1[50],name2[50];
+    cin.getline(name1,50);
+    cin.getline(name2,50);
     cout<<"###############################"<<endl;
-    cout<<"Zhang"<<endl;
+    cout<<name1<<endl;
     cout<<"Happy Birthday to you!"<<endl;
-    cout<<"      Sincerely yours Wang"<<endl;
+    cout<<"      Sincerely yours "<<name2<<endl;
     cout<<"###############################"<<endl;
     return 0;
 }
