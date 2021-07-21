6bd3efe7c880d31ef5e6ba7d4a21bd2387672e19
radiomanyi <pyzyf1983@gmail.com>
2021-07-21 08:33:47 +0800

    Asus PC

---
+++ July_21/July_21.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float annualSalary;
+    cin>>annualSalary;
+    float monthlySalary = annualSalary/12;
+    cout<<"Your monthly salary is:"<<monthlySalary;
+    system("pause>0");
+}
