4c5a6c2f4f63889f0c58249b0e0059cf7129a498
radiomanyi <pyzyf1983@gmail.com>
2021-08-05 16:26:28 +0800

    Mac Commit
    Data Type Descrption

---
+++ Aug_5/Aug_5_1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float annualSalary;
+    cout<<"Please enter your annualSalary: ";
+    cin>> annualSalary;
+    float monthlySalary = annualSalary / 12;
+    cout<<"Your monthly salary is "<<monthlySalary<<endl;
+    cout<<"In 10 years you will earn "<<annualSalary * 10<<endl;
+
+    char character ='a';
+    float firstSecondThird;
+    return 0;
+}
---
+++ Aug_5/Aug_5_2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int yearofBirth = 1995;
+    char gender = 'f';
+    bool isOlderThan18 = true;
+    float averageGrad = 4.5;
+    double balance = 45678945856;
+    cout<<"Size of int is "<<sizeof(int)<<" bytes\n";
+    cout<<"Int min value is "<<INT_MIN<<endl;
+    cout<<"Int max value is "<<INT_MAX<<endl;
+    cout<<"Size of Unsigned Int is"<<sizeof(unsigned int)<<" bytes\n";
+    cout<<"UINT max value is "<<UINT_MAX<<endl;
+    cout<<"Size of bool is "<<sizeof(bool)<<" bytes\n";
+    cout<<"Size of char is "<<sizeof(char)<<" bytes\n";
+    cout<<"Size of float is "<<sizeof(float)<<" bytes\n";
+    cout<<"Size of double is"<<sizeof(double)<<" bytes\n";
+
+}
